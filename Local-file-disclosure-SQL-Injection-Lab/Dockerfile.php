160c93c4d58f93063df9f36d180beb5241e14133
0n100 <onion@163.com>
2022-10-26 18:01:50 +0800

    一些测试修改

--- Local-file-disclosure-SQL-Injection-Lab/Dockerfile.php
+++ Local-file-disclosure-SQL-Injection-Lab/Dockerfile.php
@@ -1,11 +1,11 @@
 FROM php:7.4-fpm-alpine3.16
 
-ARG ZIPFILE=Local-file-disclosure-SQL-Injection-Lab-master
+ARG PHP_INI
 
 RUN docker-php-ext-install mysqli 
-
-#COPY $ZIPFILE /var/www/html
-   
+RUN set -ex \
+  && if [ ! -z ${PHP_INI} ]; then cp /usr/local/etc/php/${PHP_INI} /usr/local/etc/php/php.ini; fi
+  
 CMD ["php-fpm"]
 
-EXPOSE 9000
+EXPOSE 9000 
--- nginx-php-stack-base/Dockerfile.php
+++ nginx-php-stack-base/Dockerfile.php
@@ -1,7 +1,11 @@
-
 FROM php:7.4-fpm-alpine3.16
 
+ARG PHP_INI
+
 RUN docker-php-ext-install pdo_mysql
+
+RUN set -ex \
+  && if [ ! -z ${PHP_INI} ]; then cp /usr/local/etc/php/${PHP_INI} /usr/local/etc/php/php.ini; fi
 
 CMD ["php-fpm"]
 
